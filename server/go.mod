580615b054f91f176a8e3542708603410a7d811a
Woshiwuja <86830770+Woshiwuja@users.noreply.github.com>
2023-12-19 13:44:10 +0000

    Cleanup

--- server/go.mod
+++ server/go.mod
@@ -1,10 +1,3 @@
 module anarchy2036/main
 
 go 1.21.1
-
-require gorm.io/gorm v1.25.5
-
-require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-)
--- server/main.go
+++ server/main.go
@@ -1,55 +1,31 @@
 package main
 
 import (
-	"anarchy2036/db_conn"
-//	"anarchy2036/models"
-	"anarchy2036/paths"
-//	"anarchy2036/queries"
-	"fmt"
-//	"io"
-	"net/http"
-//	"os"
-//	"strings"
-//	"text/template"
+	//	"anarchy2036/models"
 
-	"gorm.io/gorm"
+	//	"anarchy2036/queries"
+	"fmt"
+	//	"io"
+	"net/http"
+	//	"os"
+	//	"strings"
+	//	"text/template"
 	//"gorm.io/driver/postgres"
 )
 
-const SRV_PORT string = ":2036"
-
-var DB *gorm.DB
-
-
+const SRV_PORT string = "8190"
+const SRV_STRING string = "http://localhost:" + SRV_PORT
 
 func main() {
 	//open server
 
-	var error error
-	DB, error = db_conn.DBConnect("host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable")
-	if error != nil {
-		fmt.Printf("problem connecting to db %v", error)
-	}
-	fmt.Printf("connected to db %v", &DB)
-
 	http.Handle("/", http.FileServer(http.Dir("../static")))
-	http.HandleFunc("/hello", paths.GetHello)
-	//http.HandleFunc("/clock", paths.GetClock)
-	http.HandleFunc("/stack", paths.GetStack)
-	http.HandleFunc("/blog/post1", paths.GetBlogPost1)
-	http.HandleFunc("/blog/post2", paths.GetBlogPost2)
-	http.HandleFunc("/blog", paths.GetBlog)
-	http.HandleFunc("/chart", paths.GetUsers(DB))
-	http.HandleFunc("/minigame", paths.GetMinigame)
-	http.HandleFunc("/minigame1", paths.GetMinigame1)
-	http.HandleFunc("/minigame2", paths.GetMinigame2)
 
-	fmt.Printf("server open on %s", SRV_PORT)
+	fmt.Printf("server open at %s", SRV_STRING)
 
-	err := http.ListenAndServe(SRV_PORT, nil)
+	err := http.ListenAndServe(":"+SRV_PORT, nil)
 	if err != nil {
 		fmt.Printf("cant open server %v", err)
 	}
-	//DB CONNECTION
 	fmt.Printf("server open on %s", SRV_PORT)
 }
